925b2ffa81869f96aeabe40fa933c01903a7ca14
Damla Ayvatas <47346126+DamlaAyvatas@users.noreply.github.com>
2023-07-03 23:53:17 +0300

    another fragment in the wall

--- app/src/main/java/com/dayvatas/fragment/BlankFragment2.kt
+++ app/src/main/java/com/dayvatas/fragment/BlankFragment2.kt
@@ -6,8 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
+class BlankFragment2 : Fragment() {
 
-class BlankFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +19,7 @@ class BlankFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank, container, false)
+        return inflater.inflate(R.layout.fragment_blank2, container, false)
     }
 
 }
